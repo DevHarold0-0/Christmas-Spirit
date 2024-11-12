15cf6d81f4b31b6c162e7adfebf33da90fb6129f
Harold <155407111+DevHarold0-0@users.noreply.github.com>
2024-11-11 21:58:51 -0500

    Add content & update navigation

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-A simple brochure site to rekindle the Christmas Spirit this holiday season.
+A fun and simple brochure site to rekindle the Christmas Spirit this holiday season.
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import '../styles/modern-normalize.css';
 import '../styles/resets.css';
 import '../styles/styles.css';
+import '../styles/components/header.css';
 import '../styles/utils.css';
